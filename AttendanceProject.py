e387d8c740ac80b475a880d14a375cb59d68b4b0
INDRA KORANGA <126662670+inderkorang@users.noreply.github.com>
2025-07-28 15:17:28 +0530

    Update AttendanceProject.py

--- AttendanceProject.py
+++ AttendanceProject.py
@@ -1,72 +1,103 @@
-
 import cv2
-import numpy as np
-import face_recognitions
+import face_recognition
+import pickle
 import os
-from datetime import datetime
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from firebase_admin import  storage
 
-path = 'Images_Attendance'
-images = []
-classNames = []
-myList = os.listdir(path)
-print(myList)
-for cl in myList:
-    curImg = cv2.imread(f'{path}/{cl}')
-    images.append(curImg)
-    classNames.append(os.path.splitext(cl)[0])
-print(classNames)
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': "",
+    'storageBucket': ""
+})
 
-def findEncodings(images):
-    encodeList =[]
-    for img in images:
+
+# Importing student images
+folderPath = 'Images'
+pathList = os.listdir(folderPath)
+print(pathList)
+imgList = []
+studentIds = []
+for path in pathList:
+    imgList.append(cv2.imread(os.path.join(folderPath, path)))
+    studentIds.append(os.path.splitext(path)[0])
+
+    fileName = f'{folderPath}/{path}'
+    bucket = storage.bucket()
+    blob = bucket.blob(fileName)
+    blob.upload_from_filename(fileName)
+
+
+    # print(path)
+    # print(os.path.splitext(path)[0])
+print(studentIds)
+
+
+def findEncodings(imagesList):
+    encodeList = []
+    for img in imagesList:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         encode = face_recognition.face_encodings(img)[0]
         encodeList.append(encode)
+
     return encodeList
 
-def markAttendance(name):
-    with open('Attendance.csv', 'r+') as f:
-        myDataList = f.readlines()
-        nameList = []
-        for line in myDataList:
-            entry = line.split(',')
-            nameList.append(entry[0])
-        if name not in nameList:
-            time_now = datetime.now()
-            tString = time_now.strftime('%H:%M:%S')
-            dString = time_now.strftime('%d/%m/%Y')
-            f.writelines(f'\n{name},{tString},{dString}')
 
-encodeListKnown = findEncodings(images)
-print('Encoding Complete')
+print("Encoding Started ...")
+encodeListKnown = findEncodings(imgList)
+encodeListKnownWithIds = [encodeListKnown, studentIds]
+print("Encoding Complete")
 
-cap = cv2.VideoCapture(0)
+file = open("EncodeFile.p", 'wb')
+pickle.dump(encodeListKnownWithIds, file)
+file.close()
+print("File Saved")
+AddDatatoDatabase.py
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
 
-while True:
-    success, img = cap.read()
-    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
-    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': ""
+})
 
-    facesCurFrame = face_recognition.face_locations(imgS)
-    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+ref = db.reference('Students')
 
-    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
-        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
-        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-        print(faceDis)
-        matchIndex = np.argmin(faceDis)
-        if matches[matchIndex]:
-            name = classNames[matchIndex].upper()
-            print(name)
-            y1, x2, y2, x1 = faceLoc
-            y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
-            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.rectangle(img, (x1, y2-35), (x2, y2), (0, 250, 0), cv2.FILLED)
-            cv2.putText(img, name, (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
-            markAttendance(name)
-    cv2.imshow('webcam', img)
-    if cv2.waitKey(10) == 13:
-        break
-cap.release()
-cv2.destroyAllWindow()
+data = {
+    "321654":
+        {
+            "name": "Murtaza Hassan",
+            "major": "Robotics",
+            "starting_year": 2017,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 4,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+    "852741":
+        {
+            "name": "Emly Blunt",
+            "major": "Economics",
+            "starting_year": 2021,
+            "total_attendance": 12,
+            "standing": "B",
+            "year": 1,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+    "963852":
+        {
+            "name": "Elon Musk",
+            "major": "Physics",
+            "starting_year": 2020,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 2,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        }
+}
 
+for key, value in data.items():
+    ref.child(key).set(value)
